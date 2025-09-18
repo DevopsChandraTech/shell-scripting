7c244ae62f0807de4cb7b109c932d86712afb859
DevOpsChandraTech <devopschandra42@gmail.com>
2025-09-18 19:00:02 +0530

    discussion with 2 friends

---
+++ 02.script.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Hello Raju,How are you"
+echo "Hello Satish, I am fine,how are you raju"
+echo "I am also good,what are you learning today"
+echo "I started devops journey today onwards"
