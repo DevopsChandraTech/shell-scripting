b508ee3400f929736ae8af07bda216d15c11ecad
DevOpsChandraTech <devopschandra42@gmail.com>
2025-09-18 19:10:32 +0530

    changing name parameters

--- 03.script.sh
+++ 03.script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #passing the parameters
-USER1=Chandra
-USER2=Ramesh
+USER1=Anu
+USER2=Gnapi
 echo "Hello $USER2,How are you"
 echo "Hello $USER1, I am fine,how are you $USER1"
 echo "I am also good,what are you learning today"
