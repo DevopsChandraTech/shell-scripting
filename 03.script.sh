2b48b3dfef4499f2d5af552a7be67e40110ff669
DevOpsChandraTech <devopschandra42@gmail.com>
2025-09-18 19:07:42 +0530

    adding variable into script

---
+++ 03.script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#passing the parameters
+USER1=Chandra
+USER2=Ramesh
+echo "Hello $USER2,How are you"
+echo "Hello $USER1, I am fine,how are you $USER1"
+echo "I am also good,what are you learning today"
+echo "I started devops journey today onwards"
