0489f942e1dcbd603727f05ea4f50836a5811450
DevOpsChandraTech <devopschandra42@gmail.com>
2025-09-23 23:34:39 +0530

    added one more line into hello-world

--- 05.read-variable.sh
+++ 05.read-variable.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 read -p "enter your username : "
-read USER_PIN # userpin is a variable
-echo -s "pls enter the userpin : $USER_PIN"
+read -s USER_PIN # userpin is a variable
+echo "pls enter the userpin : $USER_PIN"
