51a8b1f4aec2bf3d5019c24e79097e8e89ada3b7
DevOpsChandraTech <devopschandra42@gmail.com>
2025-09-23 22:44:59 +0530

    added one more line into hello-world

---
+++ 04.variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#passing arguments
+USER1="$1"
+USER2="$2"
+echo "hello $USER1 how are you"
+echo "I am good how are you $USER2"
+echo "what learns today $USER1"
+echo "I learnt devopsecops with aws $USER2"  
