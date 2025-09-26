fe091c380300cf13634f3c4f7b570c3f4b1202ca
DevOpsChandraTech <devopschandra42@gmail.com>
2025-09-27 00:32:25 +0530

    variable scripts

--- 09.date-types.sh
+++ 09.date-types.sh
@@ -2,7 +2,8 @@
 # By default shell script datatype taken script
 NUMBER1=100
 NUMBER2=200
+NAME=Chandra
 
-SUM=$(($NUMBER1 + $NUMBER2))
+SUM=$(($NUMBER1 + $NUMBER2 + $NAME))
 
 echo "the sum is : ${SUM}"
