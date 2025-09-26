5cd0ea48a46c949dd6d7e2144c41ed40024f8ef6
DevOpsChandraTech <devopschandra42@gmail.com>
2025-09-26 23:19:10 +0530

    variable scripts

--- 07.dynamicvar.sh
+++ 07.dynamicvar.sh
@@ -3,7 +3,7 @@
 #date +%y-%m-%d --> year-month-date showing
 #date +%s --> shows present seconds(ex : 1758908023)
 START_DATE=$(date +%s)
-sleep 10
+sleep 20
 END_DATE=$(date +%s)
 EXECUTION_TIME=$(($END_DATE - $START_DATE))
 echo "the script execution time is : $EXECUTION_TIME seconds"
