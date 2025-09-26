2fdfd5299c34a0c20f65ff5c3383afe7687a5524
DevOpsChandraTech <devopschandra42@gmail.com>
2025-09-26 11:21:40 +0530

    practice scripts

--- practice.sh
+++ practice.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
+# printing the first script hello-world
 echo "hello-world"
-# -----------------
+# conversations script for ravi and raju
 echo "Hello Ravi How are you"
 echo "Hey Raju I am fine How are You"
 echo "I am good ravi what are you doing now"
 echo "I am learning devops in joindevops" 
+# coversation with variables:
+USER1="Ramesh"
+USER2="Suresh"
+COURSE="DevOps"
+echo "Hello $USER1 How are you"
+echo "Hey $USER2 I am fine How are You"
+echo "I am good $USER1 what are you doing now"
+echo "I am learning $COURSE in joindevops" 
+
