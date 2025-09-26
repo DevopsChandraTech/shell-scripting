6930d8f718d2831144ea4e68e0f05a919b78caf4
DevOpsChandraTech <devopschandra42@gmail.com>
2025-09-26 23:33:53 +0530

    variable scripts

--- 08.specialvar.sh
+++ 08.specialvar.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 echo "All variables passed to the script $@"
 echo "All variables passed to the script $*"
-echo "Name of the script $0"
+echo "Name of the script : $0"
+echo "Current directory : $PWD"
+echo "Current user running : $USER"
+echo "Process Id of the script : $$"
 date +%s
