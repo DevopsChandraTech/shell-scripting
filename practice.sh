65e6a648c312f2e350761bd17496b6f50517bd86
DevOpsChandraTech <devopschandra42@gmail.com>
2025-09-26 11:14:25 +0530

    practice scripts

--- practice.sh
+++ practice.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
 echo "hello-world"
+# -----------------
+echo "Hello Ravi How are you"
+echo "Hey Raju I am fine How are You"
+echo "I am good ravi what are you doing now"
+echo "I am learning devops in joindevops" 
