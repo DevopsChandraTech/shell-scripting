d6783dd890ab0dce32235606960445f8260c5520
DevOpsChandraTech <devopschandra42@gmail.com>
2025-09-27 12:30:41 +0530

    conditions

--- 10.conditions.sh
+++ 10.conditions.sh
@@ -8,6 +8,8 @@
 NUMBER=$1
 if [ $NUMBER -lt 10 ]; then
     echo "given number less than 10"
+elif [ $NUMBER -eq 10 ]; then
+    echo "given number 10"
 else
     echo "given number greater than 10"
 fi
