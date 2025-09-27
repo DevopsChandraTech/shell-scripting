f85ee05931f76816d660ce1e07e745b2aa64f514
DevOpsChandraTech <devopschandra42@gmail.com>
2025-09-27 11:23:06 +0530

    conditions

---
+++ 10.conditions.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#if you learn devops you will get job
+#if dont learn devops you wont get job
+# -lt lessthan
+# -gt greaterthan
+# -eq equal
+# -nq not equal
+
+if [ $1 -lt 10 ] then
+    print("$1 less than 10")
+else
+    print("$1 greater than 10")
+
